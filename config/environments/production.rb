d729ed8dbc6863fea8ea1636b767359f277c8155
AbdulSalamAbro <abdussalamabro@gmail.com>
2023-08-13 13:26:24 +0500

    added devise and create view of login and signup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #here we set the production env to host on internet through huruku or other platforms
 end
